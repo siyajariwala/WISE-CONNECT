5a62cd6fb4ce9576900ec2a45582351391b63d5c
Siya Jariwala <siya.jari14@gmail.com>
2025-06-22 09:22:35 -0700

    OPTIMIZED CODEBASES

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+export function signup (rep,res){
+    res.send("Signup Route");
+}
+
+export function login(req,res){
+    res.send("Login Route");
+}
+
+export function logout(req,res){
+    res.send("Logout Route");
+}
